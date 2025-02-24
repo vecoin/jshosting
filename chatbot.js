4ccd7a921eae47f851d8aa55c4b9eb92b7ce2fc5
Francisco Alcala <alcalafrancisco@gmail.com>
2025-02-24 16:06:39 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,7 +1,7 @@
 (function() {
-    const WEBHOOK_URL = "https://hook.us1.make.com/7obx2jgtkfdp6i36mx78yamsguub13ah"; 
-    const CHATBOT_NAME = "Blickbot Assistant";  
-    const AVATAR_URL = "https://luciano234.github.io/jshosting/logo_v3_blickbot.jpg";  
+    const WEBHOOK_URL = "https://hook.us2.make.com/t3s5injva3oyrolkrpcwn459wwarcyhu"; 
+    const CHATBOT_NAME = "Blickbot Francisco";  
+    const AVATAR_URL = "https://vecoin.github.io/jshosting/logo_v3_blickbot.jpg";  
 
     // Create chat bubble
     let chatBubble = document.createElement("div");
